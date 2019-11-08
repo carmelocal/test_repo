f4181d3e26be709dd0304b7d3fbf4806bc781511
carmelocal <55434683+carmelocal@users.noreply.github.com>
2019-11-08 10:03:10 +0100

    Update kubectl-install.sh

--- kubectl-install.sh
+++ kubectl-install.sh
@@ -1,5 +1,5 @@
 sudo apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg --proxy $1 | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+grep -xq "deb https://apt.kubernetes.io/ kubernetes-xenial main" /etc/apt/sources.list.d/kubernetes.list || echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
