244eb8c0f0706decb25bda2dd89f4292223ed2e6
carmelocal <55434683+carmelocal@users.noreply.github.com>
2019-11-08 10:21:53 +0100

    Update powershell-install.sh

--- powershell-install.sh
+++ powershell-install.sh
@@ -1,5 +1,5 @@
 # Download the Microsoft repository GPG keys
-wget -q https://packages.microsoft.com/config/ubuntu/18.10/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/18.10/packages-microsoft-prod.deb -e use_proxy=yes -e http_proxy=10.72.70.116:8888
 
 # Register the Microsoft repository GPG keys
 sudo dpkg -i packages-microsoft-prod.deb
